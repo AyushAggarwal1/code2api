abaa780ef42d6dc874678b351840ac6bac4ae9a3
Ayush Aggarwal <ayushaggarwal1136@gmail.com>
2026-06-03 14:08:08 +0530

    release.yaml (#1)

--- main.go
+++ main.go
@@ -6,11 +6,22 @@ import (
 	"os"
 )
 
+var (
+	version   = "dev"
+	buildDate = "unknown"
+)
+
 func main() {
 	path := flag.String("path", ".", "Directory to scan")
 	output := flag.String("output", "", "Output JSON file path (default: stdout only)")
 	verbose := flag.Bool("verbose", false, "Verbose output")
+	ver := flag.Bool("version", false, "Print version and exit")
 	flag.Parse()
+
+	if *ver {
+		fmt.Printf("code2api %s (built %s)\n", version, buildDate)
+		os.Exit(0)
+	}
 
 	if *path == "" {
 		fmt.Fprintln(os.Stderr, "Error: -path required")
